1ec2d56fe3afba5a69a5a0d7b1ba95fad7df26bc
Shoon <tshoonlai@gmail.com>
2025-12-18 21:19:57 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import OurLegacy from "./Pages/Legacy";
 import CareGuide from "./Pages/CareGuide"; // ✅ default import
-<<<<<<< HEAD
 import HomePage from "./Pages/Home";
 // import CollectionPage from "./CollectionPage";
 // import FishDetailsPage from "./FishDetailsPage";
 
-=======
 import FAQ from "./Pages/FAQ";
->>>>>>> 007de5991eb1570f325cfdf64364e530c84bc8b7
 
 function App() {
   return (
@@ -24,14 +21,11 @@ function App() {
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/legacy" element={<OurLegacy />} />
         <Route path="/care-guides" element={<CareGuide />} />{" "}
-<<<<<<< HEAD
         <Route path="/" element={<HomePage />} />
       {/* <Route path="/collection" element={<CollectionPage />} />
       <Route path="/fish/:id" element={<FishDetailsPage />} /> */}
 
-=======
         <Route path="/faq" element={<FAQ />} />{" "}
->>>>>>> 007de5991eb1570f325cfdf64364e530c84bc8b7
         {/* ✅ matches default export */}
       </Routes>
 
